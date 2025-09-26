3648d56a703c2c0a50542cd2c5198e42c4ef114e
Charbel Youssef <charbel.youssef@foo.mobi>
2025-09-26 14:11:00 +0300

    Updates

---
+++ Sources/SPMTest/SPMTest.swift
@@ -0,0 +1,9 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public struct SPMTest {
+    public init() {}
+    public func greet(name: String) -> String {
+        "Hello, \(name)!"
+    }
+}
